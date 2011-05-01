29f18bbb94f6e75e1684468e77c602baebbbec9c
John Van Enk <vanenkj@gmail.com>
2011-05-01 15:59:54 -0400

    Added imports and outputs.

--- src/Test/Volcano.hs
+++ src/Test/Volcano.hs
@@ -1,1 +1,10 @@
-module Test.Volcano ( ) where
+module Test.Volcano (
+    module Test.Volcano.Data,
+    module Test.Volcano.Search,
+    module Test.Volcano.Parse.Haskell
+) where
+
+import Test.Volcano.Data
+import Test.Volcano.Search
+import Test.Volcano.Parse.Haskell
+
